917e791529ded325ed4b7bcd61e8a9824495806b
+Sharon <+wanjiku.kahira@gmail.com>
2025-05-08 12:19:21 +0300

    Create product.py model

---
+++ server/models/product.py
@@ -0,0 +1,41 @@
+from models.db import db
+from sqlalchemy_serializer import SerializerMixin
+from datetime import datetime
+
+class Product(db.Model, SerializerMixin):
+    """
+    Product model represents items listed by sellers. Includes metadata, pricing, and inventory.
+
+    Attributes:
+        id (int): Primary key.
+        seller_id (int): Foreign key to the User.
+        name (str): Product name.
+        description (str): Description of the product.
+        category (str): Product category.
+        price_sats (int): Price in satoshis.
+        img_url (str): Image URL.
+        stock_quantity (int): Inventory count.
+        created_at (datetime): Timestamp when product was added.
+
+    Relationships:
+        seller: User who listed the product.
+        carts: Carts that contain the product.
+        order_details: Orders that include this product.
+    """
+    __tablename__ = 'products'
+
+    serialize_rules = ('-seller.products','-carts')
+
+    id = db.Column(db.Integer, primary_key=True)
+    seller_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    name = db.Column(db.String(100))
+    description = db.Column(db.Text)
+    category = db.Column(db.String(100))
+    price_sats = db.Column(db.BigInteger)
+    img_url = db.Column(db.String(255))
+    stock_quantity = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime)
+
+    seller = db.relationship('User', back_populates='products')
+    carts = db.relationship('Cart', back_populates='product', cascade="all, delete-orphan")
+    order_details = db.relationship('OrderDetail', back_populates='product', cascade="all, delete-orphan")
